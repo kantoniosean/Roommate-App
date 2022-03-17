5f6f4fbcd654b4803712c908ab3e9351388a6000
Kantonio Brownlee <kantoniosean@yahoo.com>
2022-03-16 22:07:16 -0500

    changes to login and html

--- src/front-end/LoginPage.js
+++ src/front-end/LoginPage.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+// import style from './style.css';
 
 function LoginPage() {
-
+    return(        
+        <html style={{background: "#F26666"}}></html>
+    )
 }
 
 export default LoginPage

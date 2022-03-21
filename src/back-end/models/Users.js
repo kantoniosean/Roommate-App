4a4ac00458a1d30475afb29944a48ab8c4390178
Quinn York <qyork@ncsu.edu>
2022-03-21 13:46:57 -0500

    added functions to server.js

--- src/back-end/models/Users.js
+++ src/back-end/models/Users.js
@@ -1,25 +1,21 @@
 const mongoose = require('mongoose');
+const chore = require('../Chore');
 
 const UserSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
     },
     lastName: {
         type: String,
-        required: true,
     },
     id: {
         type: String,
-        required: true,
     },
     email: {
         type: String,
-        required: true,
     },
     results: {
         type: [String],
-        required: false,
     }
     /*roommates: {
         type: [User],

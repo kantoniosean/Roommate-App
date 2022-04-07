6bda7d9a4854ff300931da016f7163d36216a1fa
eejohnson7 <78312371+eejohnson7@users.noreply.github.com>
2022-04-06 21:46:35 -0500

    Update userBuilder.js

--- src/back-end/userBuilder.js
+++ src/back-end/userBuilder.js
@@ -4,21 +4,10 @@ class UserBuilder {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
-        this.email = email;
     }
-
-    results([results]) {
-        this.results = results;
-        return this;
-    }
-
+    
     roommates(r) {
         this.roommates = roommates;
-        return this;
-    }
-
-    chores(c) {
-        this.chores = chores;
         return this;
     }
 

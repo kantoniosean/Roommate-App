91ab25f259b04c91fa9a3dc741430f15de2f211d
eejohnson7 <78312371+eejohnson7@users.noreply.github.com>
2022-04-07 00:25:09 -0500

    Update Preferences.js

--- frontend/src/Preferences.js
+++ frontend/src/Preferences.js
@@ -10,18 +10,39 @@ import './RoommateFinder';
 import './Settings';
 import './ChoreList';
 
-function Preferences() {
+function Preferences () {
+  const [preferences, setPreferences] = ([]);
+  const[_id, set_id] = useState("");
+  const[name, setName] = useState("");
+  const[username, setUsername] = useState("");
+  const[roomies, setRoomies] = useState([]);
+
+  /*need to test with actual currentUser, but it works with dummy data*/
+  var currentUser = {  "_id": "624cce80ce757e25629f9879",  "name": "Erin",  "username": "eejohnson",  "roomies": [],  "matches": [    "Pedro"  ],  "preferences": [    "hobbies",    "games"  ]};
+  var questions = ["ifHouse", "ifApt", "ifPets", "ifDrinker", "ifSmoker", "ifPartier", "ifMornings", "ifNights"];
+  var answers = [];
+
   const updatePreferences = () => {
-    var answers = document.getElementsByTagName("input");
-    //need to figure out how to put actual string answers into array
-    var preferences = Array.from(answers);
-    
-    /*Axios.post("http://localhost:3001/updatePreferences", {
-      preferences
-    }).then((response) => {*/
+    answers = questions.map(function(question){
+      console.log(question + ": " + document.querySelector("input[name='" + question + "']:checked").id);
+      return document.querySelector("input[name='" + question + "']:checked").id;
+    });
+      
+    setPreferences(answers);
+    set_id(currentUser._id);
+    setName(currentUser.name);
+    setUsername(currentUser.username);
+    setRoomies(currentUser.roomies);
+
+    Axios.post("http://localhost:3001/updatePreferences", {
+      preferences,
+      _id,
+      name,
+      username,
+      roomies
+    }).then((response) => {
     alert("PREFERENCES UPDATED"); 
-    console.log(preferences);
-    //}); 
+    }); 
   }
 
   return(
@@ -29,20 +50,21 @@ function Preferences() {
       <img src={logo} alt="logo" height="75"></img>
       <p></p>
 
-      <div class="row col-5">
+      <div>
         <h4 class="fw-bold text-center mt-3"> </h4>
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Are you looking to rent a house?</p>
+
           <div class="form-check mb-2">
-            <input class="form-check-input" type="radio" name="ifHouse" id="house" />
-            <label class="form-check-label" for="house">
+            <input class="form-check-input" type="radio" name="ifHouse" id="house"/>
+            <label for="house">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifHouse" id="notHouse" />
-            <label class="form-check-label" for="notHouse">
+            <label for="notHouse">
               No
             </label>
           </div>
@@ -50,15 +72,16 @@ function Preferences() {
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Are you looking to rent an apartment?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifApt" id="apt" />
-            <label class="form-check-label" for="apt">
+            <label for="apt">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifApt" id="notApt" />
-            <label class="form-check-label" for="notApt">
+            <label for="notApt">
               No
             </label>
           </div>
@@ -66,15 +89,16 @@ function Preferences() {
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Are you okay with pets?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifPets" id="pets" />
-            <label class="form-check-label" for="pets">
+            <label for="pets">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifPets" id="pets" />
-            <label class="form-check-label" for="noPets">
+            <label for="noPets">
               No
             </label>
           </div>
@@ -82,15 +106,16 @@ function Preferences() {
   
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Do you drink?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifDrinker" id="drinker" />
-            <label class="form-check-label" for="drinker">
+            <label for="drinker">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifDrinker" id="nonDrinker" />
-            <label class="form-check-label" for="nonDrinker">
+            <label for="nonDrinker">
               No
             </label>
           </div>
@@ -98,15 +123,16 @@ function Preferences() {
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Do you smoke?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifSmoker" id="smoker" />
-            <label class="form-check-label" for="smoker">
+            <label for="smoker">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifSmoker" id="nonSmoker" />
-            <label class="form-check-label" for="nonSmoker">
+            <label for="nonSmoker">
               No
             </label>
           </div>
@@ -114,15 +140,16 @@ function Preferences() {
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Are you okay with having people over?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifPartier" id="partier" />
-            <label class="form-check-label" for="partier">
+            <label for="partier">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifPartier" id="nonPartier" />
-            <label class="form-check-label" for="nonPartier">
+            <label for="nonPartier">
               No
             </label>
           </div>
@@ -130,15 +157,16 @@ function Preferences() {
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Are you a morning person?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifMornings" id="mornings" />
-            <label class="form-check-label" for="mornings">
+            <label for="mornings">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
-            <input class="form-check-input" type="radio" name="ifMornings" id="nonMornings" />
-            <label class="form-check-label" for="nonMornings">
+            <label> 
+              <input class="form-check-input" type="radio" name="ifMornings" id="nonMornings" />
               No
             </label>
           </div>
@@ -146,23 +174,25 @@ function Preferences() {
 
         <form style={{color:"#F2C4C4"}}>
           <p class="fw-bold">Are you a night owl?</p>
+
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifNights" id="nights" />
-            <label class="form-check-label" for="mornings">
+            <label for="mornings">
               Yes
             </label>
           </div>
           <div class="form-check mb-2">
             <input class="form-check-input" type="radio" name="ifNights" id="nonNights" />
-            <label class="form-check-label" for="nonNights">
+            <label for="nonNights">
               No
             </label>
           </div>
-        </form>
-      </div>
 
-      <div class="col-md-12 text-center">
-        <Button class="rounded" variant="danger" onClick={updatePreferences}> <a href="./RoommateFinder"></a>Submit</Button>
+          <div class="col-md-12 text-center">
+            <Button class="rounded" variant="danger" onClick={updatePreferences}>Update Preferences</Button>
+          </div>
+        </form>
+        <br></br>
       </div>
 
       <div>
